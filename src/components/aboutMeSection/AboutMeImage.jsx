959912022df9c218dd3c1d6f78da07c35fdbe074
UDUKUNDA <udukundaelysee@gmail.com>
2025-07-27 23:52:52 +0200

    fix-vid

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -4,7 +4,7 @@ const AboutMeVideo = () => {
       {/* Video Container with Rounded Corners */}
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <video
-          src="../../videos/elvpitch.mp4" // Make sure this path is correct
+          src="../../videos/elvpitc.mp4" // Make sure this path is correct
           className="h-full w-auto object-cover"
           controls
           playsInline
